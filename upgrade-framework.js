2a2cbe9e662ada76fa42619edb405900f27acd16
Thomas Taylor <thomas.taylor@kineo.com>
2016-04-20 15:51:59 +0100

    no message

--- upgrade-framework.js
+++ upgrade-framework.js
@@ -1,5 +1,8 @@
 var app = require('./lib/application')();
+var exec = require('child_process').exec;
 var fs = require('fs-extra');
+
+var config = require('./conf/config.json');
 
 app.run({
   skipVersionCheck: true,
@@ -7,24 +10,41 @@ app.run({
 });
 
 app.on('serverStarted', function () {
-  var opts = {
-    cwd: 'temp/' + configFile.masterTenantID + '/adapt_framework',
-    stdio: [0, 'pipe', 'pipe']
-  }
-  exec('git fetch origin', opts)
-    .stdout.on('data', console.log)
-    .stderr.on('data', console.log)
-    .on('exit', function (error, stdout, stderr) {
-      if (error) return callback(error);
-      exec('git reset --hard ' + tagName + ' && npm install', opts)
-        .stdout.on('data', console.log)
-        .stderr.on('data', console.log)
-        .on('exit', function (error, stdout, stderr) {
-          if (error) return exitUpgrade(error);
-          rimraf(configFile.root + opts.cwd + '/src/course', callback);
-        });
-    });
+  getLatestGitVersion('temp/' + config.masterTenantID + '/adapt_framework', function(error, version) {
+    console.log(error, version);
+  });
 });
+
+function getLatestGitVersion(dir, callback) {
+  var opts = {
+    cwd: dir,
+    stdio: [0, 'pipe', 'pipe']
+  };
+  var c1 = exec('git fetch --tags origin', opts);
+  c1.stdout.on('data', console.log);
+  c1.on('exit', function (error, stdout, stderr) {
+    if (error) return callback(error);
+    var tags = "";
+    var c2 = exec('git tag', opts);
+    c2.stdout.on('data', function(data) {
+      tags += data;
+    });
+    c2.stderr.on('data', console.log);
+    c2.on('exit', function (error) {
+      if (error) return callback(error);
+      var tagsArr = tags.split('\n');
+      callback(null, tagsArr[tagsArr.length-2]);
+    });
+  });
+}
+
+function installGitTag(dir, tag, callback) {
+  var child = exec('git reset --hard ' + latestFramework + ' && npm install', opts);
+  child.stdout.on('data', console.log);
+  child.on('exit', function (error, stdout, stderr) {
+    if (error) return exitUpgrade(error);
+  });
+}
 
 function exitUpgrade(error, code) {
   console.log(error || 'Framework upgraded successfully!');
